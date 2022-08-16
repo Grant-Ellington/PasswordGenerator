e3cd259f4b128d0d81c97ab1dd47d288587f81e0
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-16 18:45:41 -0500

    number showing but just one;need to debugg

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,33 @@
 // Assignment code here
-
+function passwordGenerator(){
 var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 12;
-var password = ""
+var passwordLength = window.prompt('Choose between 8 and 128 charcters');
+var passwordText = "";
 
-var passwordGenerator = function (){ 
-  for ( var i = 0; i<= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random()*chars.length);
-  password += chars.substring(randomNumber, randomNumber +1)
-}         
-}
-passwordGenerator();
 
-console.log(password)
+  for( i = 0; i =10; i++) {
+  var randomNumber= Math.floor(Math.random()*passwordLength)
+  passwordText += chars.substring(randomNumber, randomNumber +1)
+  return passwordText
+  }    
+  console.log(randoomNumber)
+  debugger
+};
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
+
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(){
+  var password = passwordGenerator();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
