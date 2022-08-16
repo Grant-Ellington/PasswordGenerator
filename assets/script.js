44c19794d99eabec616243b06e6bb9ec2eda3152
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-16 14:24:06 -0500

    password prints to console;still need to add to text box

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,19 @@ var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTU
 var passwordLength = 12;
 var password = ""
 
-for( var i = 0; i<= passwordLength; i++) {
+var passwordGenerator = function (){ 
+  for ( var i = 0; i<= passwordLength; i++) {
   var randomNumber = Math.floor(Math.random()*chars.length);
   password += chars.substring(randomNumber, randomNumber +1)
-};
+}         
+}
+passwordGenerator();
+
 console.log(password)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
