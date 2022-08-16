dffe457ae763a07b3822a583540b6255d430399f
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-16 13:51:55 -0500

    added js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
+var password = ""
+
+for( var i = 0; i<= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random()*chars.length);
+  password += chars.substring(randomNumber, randomNumber +1)
+};
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
