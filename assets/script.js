1b1b6a741e43c07cd3253dd22b9f677e4dd0fed8
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-16 14:07:02 -0500

    connection to js script completed; passwordscript running; Next connect password script to text field

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ for( var i = 0; i<= passwordLength; i++) {
   var randomNumber = Math.floor(Math.random()*chars.length);
   password += chars.substring(randomNumber, randomNumber +1)
 };
-
+console.log(password)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
