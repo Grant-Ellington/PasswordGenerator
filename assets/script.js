09400134e0b027e1e1da040ad7dab94fbe7e2cad
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-18 15:53:09 -0500

    Created Readme

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,26 @@
-
+// this function creates the generator. passwordLength is determined by prompt-any length will work from 1-2,147,647 charcters; but it is not recommended; passwordText is undefined until the for loop runs. 
 function genPassword() {
-  var passwordLength = window.prompt('Choose between 8 and 128 charcters');
+  var passwordLength = window.prompt('Choose between 8 and 128 charcters')
   var chars = charSel();
   var passwordText = "";
-
+//below is the for loop to generate password. 
   for (i = 0; i <= passwordLength; i++) {
   var randomNumber = Math.floor(Math.random() * chars.length);
   passwordText += chars.substring(randomNumber, randomNumber + 1);
 } return passwordText
 }
 
-
+// selects froma variety of charcters to use.
 function charSel() {
   let lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
   let upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let numbers = '1234567890';
   let specialChar = '!@#$%^&*(),./<>?';
 
-  var useLowerCase = window.confirm('Press OK if you WANT LOWER CASE LETTERS in your password.\n\n Press CANCEL if you DO NOT want LOWER CASE LETTERS in your password');
+  var useLowerCase = window.confirm('Press OK if you WANT LOWER CASE LETTERS in your password.\n\nPress CANCEL if you DO NOT want LOWER CASE LETTERS in your password');
   var useCapitol = window.confirm('Press OK if you WANT CAPITOL LETTERS in you password.\n\nPress CANCEL if you DO NOT want CAPITOL LETTERS in you password.');
   var useNumbers = window.confirm('Press OK if you WANT NUMBERS in your password.\n\nPress CANCEL if you DO NOT want NUMBERS in your password.');
-  var useSpecialSpecialChar = window.confirm('Press OK if you WANT SPECIAL CHARACTERS (--!@#$%^&*(),./<>?--) in your password.\n Press CANCEL if tyou DO NOT want SPECIAL CHARACTERS in your password');
+  var useSpecialSpecialChar = window.confirm('Press OK if you WANT SPECIAL CHARACTERS (--!@#$%^&*(),./<>?--) in your password.\n\nPress CANCEL if tyou DO NOT want SPECIAL CHARACTERS in your password');
 
   if (useLowerCase === true && useCapitol === true && useNumbers === true && useSpecialSpecialChar === true) {
     let chars = lowerCaseChar + upperCaseChar + numbers + specialChar;
@@ -68,15 +68,15 @@ function charSel() {
     let chars = specialChar;
     return chars;
   } else {
-    window.confirm('You have not choosen any valid character type.\n Please reload page to begin again.');
+    window.confirm('You have not choosen any valid character type.\nPress OK or Cancel to restart password generation process');
   };
 
 };
 
-// Get references to the #generate element
+// Gets references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = genPassword();
   var passwordText = document.querySelector("#password");
@@ -85,6 +85,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Adds event listener to generate button; listens to button click
 generateBtn.addEventListener("click", writePassword);
 
