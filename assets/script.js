63a6398b7785c330fc1f85d85e5d268d49f191d0
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-18 16:01:37 -0500

    doubled checked special charcters

--- assets/script.js
+++ assets/script.js
@@ -15,12 +15,12 @@ function charSel() {
   let lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
   let upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let numbers = '1234567890';
-  let specialChar = '!@#$%^&*(),./<>?';
+  let specialChar = '!\"#$%&\'()*+,-./:;<=>?@[]^_`|}~';
 
   var useLowerCase = window.confirm('Press OK if you WANT LOWER CASE LETTERS in your password.\n\nPress CANCEL if you DO NOT want LOWER CASE LETTERS in your password');
   var useCapitol = window.confirm('Press OK if you WANT CAPITOL LETTERS in you password.\n\nPress CANCEL if you DO NOT want CAPITOL LETTERS in you password.');
   var useNumbers = window.confirm('Press OK if you WANT NUMBERS in your password.\n\nPress CANCEL if you DO NOT want NUMBERS in your password.');
-  var useSpecialSpecialChar = window.confirm('Press OK if you WANT SPECIAL CHARACTERS (--!@#$%^&*(),./<>?--) in your password.\n\nPress CANCEL if tyou DO NOT want SPECIAL CHARACTERS in your password');
+  var useSpecialSpecialChar = window.confirm('Press OK if you WANT SPECIAL CHARACTERS (--)(!"#$%&\'*+,-./:;<=>?@^_\`|}~][--) in your password.\n\nPress CANCEL if tyou DO NOT want SPECIAL CHARACTERS in your password');
 
   if (useLowerCase === true && useCapitol === true && useNumbers === true && useSpecialSpecialChar === true) {
     let chars = lowerCaseChar + upperCaseChar + numbers + specialChar;
